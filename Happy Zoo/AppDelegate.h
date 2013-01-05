cb2186bcae160776d557d0caaef99e31c3ad346e
f <ff@fs-Mac.local>
2013-01-05 23:51:23 +0800

    Initial Commit

---
+++ Happy Zoo/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  Happy Zoo
+//
+//  Created by f on 1/5/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+	
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
